a5a687c01f55d6d34bd56bccccfd1a706b0979c7
newone111 <newone111@hotmail.com>
2019-05-07 00:55:09 +0300

    adding js files

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+const express = require('express');
+const { fork } = require('child_process');
 const app = express();
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/intense', (req, res) => {
-  const worker = fork('./worker');
-  worker.on('message', ({ fruit }) => {
-    res.send(`Hello Intense ${fruit}!`);
-    worker.kill();
-  });
-  worker.send({ letter: 'a' });
+    var sys = require('sys')
+    var exec = require('child_process').exec;
+
+    function puts(error, stdout, stderr) { sys.puts(stdout) }
+    exec("seq 1 | xargs -P0 -n1 timeout 15 yes > /dev/null", function(err, stdout, stderr) {
+    console.log(stdout);
+    });
+
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
