f290d84a600d0737519d4e7cc2af7e7283ce2169
newone111 <newone111@hotmail.com>
2019-05-06 23:10:56 +0300

    adding js files

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/intense', (req, res) => {
+  const worker = fork('./worker');
+  worker.on('message', ({ fruit }) => {
+    res.send(`Hello Intense ${fruit}!`);
+    worker.kill();
+  });
+  worker.send({ letter: 'a' });
+});
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
