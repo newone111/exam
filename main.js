fead1aa370f7786295dc5a4c1f93b0028eceb663
newone111 <newone111@hotmail.com>
2019-05-08 18:42:20 +0300

    adding js files 123

--- main.js
+++ main.js
@@ -7,11 +7,9 @@ app.get('/intense', (req, res) => {
     res.send('CPU Load test for 60 dec');
     var sys = require('sys');
     var exec = require('child_process').exec;
-
     function puts(error, stdout, stderr) { sys.puts(stdout) }
     exec("seq 1 | xargs -P0 -n1 timeout 60 yes > /dev/null", function(err, stdout, stderr) {
     console.log(stdout);
     });
 });
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
